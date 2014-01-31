28ebd21cb2868b4ddef46dc06495036080187b5f
Alex McTammany <apmctammany@gmail.com>
2014-01-31 02:54:22 -0500

    fuck dashed and solid backings

--- app/scripts/chemistry/services/bond.js
+++ app/scripts/chemistry/services/bond.js
@@ -24,6 +24,12 @@ angular.module('mctApp')
       return atom === this.startAtom ? this.endAtom : this.startAtom;
     };
 
+    Bond.prototype.getTopAtom = function () {
+      return (this.startAtom.position.z - this.endAtom.position.z > 0) ? this.startAtom : this.endAtom;
+    };
+    Bond.prototype.getBottomAtom = function () {
+      return this.getOtherAtom(this.getTopAtom());
+    };
     Bond.prototype.select = function () {
       this.selected = true;
     };
@@ -57,13 +63,26 @@ angular.module('mctApp')
 
     };
     Bond.prototype.draw = function (ctx) {
+      var a1 = this.getTopAtom();
+      a1.getScreenPosition();
+      var a2 = this.getBottomAtom();
+      a2.getScreenPosition();
       ctx.beginPath();
       ctx.strokeStyle = this.selected ? 'red' : 'black';
-      this.startAtom.getScreenPosition();
-      this.endAtom.getScreenPosition();
+      var dz = a1.position.z - a2.position.z;
+      //if (dz > 0.15) {
+        //ctx.lineWidth = 3;
+        //ctx.setLineDash([]);
+      //} else if (dz < 0.15) {
+        //ctx.lineWidth = 1;
+        //ctx.setLineDash([5]);
+      //} else {
+        //ctx.lineWidth = 1;
+        //ctx.setLineDash([]);
+      //}
       for (var i = 1; i <= this.order; i++) {
-        ctx.moveTo(this.startAtom.screenX + (i * 2 - 1), this.startAtom.screenY + i * 2 - 1);
-        ctx.lineTo(this.endAtom.screenX + i * 2 - 1, this.endAtom.screenY + i * 2 - 1);
+        ctx.moveTo(a1.screenX + (i * 2 - 1), a1.screenY + i * 2 - 1);
+        ctx.lineTo(a2.screenX + i * 2 - 1, a2.screenY + i * 2 - 1);
       }
       ctx.closePath();
       ctx.stroke();
