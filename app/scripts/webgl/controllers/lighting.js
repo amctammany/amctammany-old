264b9b8f9df6b853da8199850fdf442ee405502e
Alex McTammany <apmctammany@gmail.com>
2014-03-03 16:51:47 -0500

    added bitwise to jshint and fixed lighting ctrl

--- app/scripts/webgl/controllers/lighting.js
+++ app/scripts/webgl/controllers/lighting.js
@@ -91,7 +91,7 @@ angular.module('mctApp')
 
     function setupWebGL () {
       gl.clearColor(0.1, 0.5, 0.1, 1.0);
-      gl.clear(gl.COLOR_BUFFER_BIT);
+      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
       gl.enable(gl.DEPTH_TEST);
 
       gl.viewport(0, 0, canvas.width, canvas.height);
